8d487ee4a3aab4b0209f3fe084c6f58a0cd2352a
al-burlachenko <76491243+al-burlachenko@users.noreply.github.com>
2022-05-10 17:56:26 +0000

    Deploying to gh-pages from @ al-burlachenko/drill-project@52760d165196aed0281cbb1e395830766a140352 🚀

--- src.a15bffe9.js
+++ src.a15bffe9.js
@@ -1,6 +1,8 @@
 parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"clu1":[function(require,module,exports) {
 
-},{"./../images/svg/check.svg":[["check.9d4e7bb1.svg","eyF3"],"eyF3"],"./../images/svg/product-specs-list-style.svg":[["product-specs-list-style.51dc3bdb.svg","FmDF"],"FmDF"]}],"Focm":[function(require,module,exports) {
-"use strict";require("./sass/main.scss");
-},{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/drill-project/src.cea85f14.js.map
+},{"./../images/svg/check.svg":[["check.9d4e7bb1.svg","eyF3"],"eyF3"],"./../images/svg/product-specs-list-style.svg":[["product-specs-list-style.51dc3bdb.svg","FmDF"],"FmDF"]}],"K7rJ":[function(require,module,exports) {
+const e=document.querySelector("body"),t=document.querySelector(".header-submenu"),l=document.querySelector(".menu-btn"),s=document.querySelector(".header__menu-layout"),n=document.querySelector(".header__contact"),o=()=>{t.style.display="none"===t.style.display||""===t.style.display?"block":"none"},c=()=>{s.classList.toggle("is-open"),l.firstElementChild.classList.toggle("is-open"),l.lastElementChild.classList.toggle("is-open"),e.style.overflow=s.classList.contains("is-open")?"hidden":""};t.previousElementSibling.addEventListener("click",o),l.addEventListener("click",c);
+},{}],"Focm":[function(require,module,exports) {
+"use strict";require("./sass/main.scss"),require("./js/submenu");
+},{"./sass/main.scss":"clu1","./js/submenu":"K7rJ"}]},{},["Focm"], null)
+//# sourceMappingURL=/drill-project/src.a15bffe9.js.map
